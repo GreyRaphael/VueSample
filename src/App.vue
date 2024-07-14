681dea0bc23ef86261ad758f110d756359db2872
BeGeWei <grey@pku.edu.cn>
2024-07-14 20:45:52 +0800

    add datatable components

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,9 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </header>
 
-  <RouterView />
+  <n-message-provider>
+    <RouterView />
+  </n-message-provider>
 </template>
 
 <style scoped>
